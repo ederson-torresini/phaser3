2c2b04f42521b30d573fdf524ceac0224c680b80
Ederson Torresini <boidacarapreta@gmail.com>
2019-10-08 15:28:49 -0300

    Suporte a tela cheia

--- js/index.js
+++ js/index.js
@@ -11,6 +11,13 @@ var config = {
             debug: false
         }
     },
+    scale: {
+        mode: Phaser.Scale.FIT,
+        parent: "game",
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 800,
+        height: 600
+    },
     scene: {
         preload: preload,
         create: create,
@@ -37,6 +44,10 @@ function preload() {
     this.load.spritesheet('dude', 'assets/dude.png', {
         frameWidth: 32,
         frameHeight: 48
+    });
+    this.load.spritesheet("fullscreen", "assets/fullscreen.png", {
+        frameWidth: 64,
+        frameHeight: 64
     });
 }
 
@@ -131,6 +142,44 @@ function create() {
     this.physics.add.overlap(player, stars, collectStar, null, this);
 
     this.physics.add.collider(player, bombs, hitBomb, null, this);
+
+    // Botão de ativar/desativar tela cheia
+    var button = this.add
+        .image(800 - 16, 16, "fullscreen", 0)
+        .setOrigin(1, 0)
+        .setInteractive();
+
+    // Ao clicar no botão de tela cheia
+    button.on(
+        "pointerup",
+        function () {
+            if (this.scale.isFullscreen) {
+                button.setFrame(0);
+                this.scale.stopFullscreen();
+            } else {
+                button.setFrame(1);
+                this.scale.startFullscreen();
+            }
+        },
+        this
+    );
+
+    // Tecla "F" também ativa/desativa tela cheia
+    var FKey = this.input.keyboard.addKey("F");
+    FKey.on(
+        "down",
+        function () {
+            if (this.scale.isFullscreen) {
+                button.setFrame(0);
+                this.scale.stopFullscreen();
+            } else {
+                button.setFrame(1);
+                this.scale.startFullscreen();
+            }
+        },
+        this
+    );
+
 }
 
 function update() {
