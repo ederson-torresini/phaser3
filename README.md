3beb1dcb2eb7b40ad7a28dcd6dcc75b59e1dfc99
Ederson Torresini <boidacarapreta@gmail.com>
2019-10-09 00:54:32 -0300

    Aplicar Prettier em todos os arquivos de texto

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # _Making your first Phaser 3 game_
+
 O primeiro jogo feito em sala foi o tutorial [_Making your first Phaser 3 game_](https://phaser.io/tutorials/making-your-first-phaser-3-game) do site oficial.
--- js/index.js
+++ js/index.js
@@ -1,28 +1,28 @@
 var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: {
-                y: 300
-            },
-            debug: false
-        }
-    },
-    scale: {
-        mode: Phaser.Scale.FIT,
-        parent: "game",
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: 800,
-        height: 600
-    },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: {
+        y: 300
+      },
+      debug: false
     }
+  },
+  scale: {
+    mode: Phaser.Scale.FIT,
+    parent: "game",
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 800,
+    height: 600
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
 };
 
 var player;
@@ -37,207 +37,209 @@ var scoreText;
 var game = new Phaser.Game(config);
 
 function preload() {
-    this.load.image('sky', 'assets/sky.png');
-    this.load.image('ground', 'assets/platform.png');
-    this.load.image('star', 'assets/star.png');
-    this.load.image('bomb', 'assets/bomb.png');
-    this.load.spritesheet('dude', 'assets/dude.png', {
-        frameWidth: 32,
-        frameHeight: 48
-    });
-    this.load.spritesheet("fullscreen", "assets/fullscreen.png", {
-        frameWidth: 64,
-        frameHeight: 64
-    });
+  this.load.image("sky", "assets/sky.png");
+  this.load.image("ground", "assets/platform.png");
+  this.load.image("star", "assets/star.png");
+  this.load.image("bomb", "assets/bomb.png");
+  this.load.spritesheet("dude", "assets/dude.png", {
+    frameWidth: 32,
+    frameHeight: 48
+  });
+  this.load.spritesheet("fullscreen", "assets/fullscreen.png", {
+    frameWidth: 64,
+    frameHeight: 64
+  });
 }
 
 function create() {
-    //  A simple background for our game
-    this.add.image(400, 300, 'sky');
+  //  A simple background for our game
+  this.add.image(400, 300, "sky");
 
-    //  The platforms group contains the ground and the 2 ledges we can jump on
-    platforms = this.physics.add.staticGroup();
+  //  The platforms group contains the ground and the 2 ledges we can jump on
+  platforms = this.physics.add.staticGroup();
 
-    //  Here we create the ground.
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+  //  Here we create the ground.
+  //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+  platforms
+    .create(400, 568, "ground")
+    .setScale(2)
+    .refreshBody();
 
-    //  Now let's create some ledges
-    platforms.create(600, 400, 'ground');
-    platforms.create(50, 250, 'ground');
-    platforms.create(750, 220, 'ground');
+  //  Now let's create some ledges
+  platforms.create(600, 400, "ground");
+  platforms.create(50, 250, "ground");
+  platforms.create(750, 220, "ground");
 
-    // The player and its settings
-    player = this.physics.add.sprite(100, 450, 'dude');
+  // The player and its settings
+  player = this.physics.add.sprite(100, 450, "dude");
 
-    //  Player physics properties. Give the little guy a slight bounce.
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
+  //  Player physics properties. Give the little guy a slight bounce.
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
 
-    //  Our player animations, turning, walking left and walking right.
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', {
-            start: 0,
-            end: 3
-        }),
-        frameRate: 10,
-        repeat: -1
-    });
+  //  Our player animations, turning, walking left and walking right.
+  this.anims.create({
+    key: "left",
+    frames: this.anims.generateFrameNumbers("dude", {
+      start: 0,
+      end: 3
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
 
-    this.anims.create({
-        key: 'turn',
-        frames: [{
-            key: 'dude',
-            frame: 4
-        }],
-        frameRate: 20
-    });
+  this.anims.create({
+    key: "turn",
+    frames: [
+      {
+        key: "dude",
+        frame: 4
+      }
+    ],
+    frameRate: 20
+  });
 
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', {
-            start: 5,
-            end: 8
-        }),
-        frameRate: 10,
-        repeat: -1
-    });
+  this.anims.create({
+    key: "right",
+    frames: this.anims.generateFrameNumbers("dude", {
+      start: 5,
+      end: 8
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
 
-    //  Input Events
-    cursors = this.input.keyboard.createCursorKeys();
+  //  Input Events
+  cursors = this.input.keyboard.createCursorKeys();
 
-    //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
-    stars = this.physics.add.group({
-        key: 'star',
-        repeat: 11,
-        setXY: {
-            x: 12,
-            y: 0,
-            stepX: 70
-        }
-    });
+  //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
+  stars = this.physics.add.group({
+    key: "star",
+    repeat: 11,
+    setXY: {
+      x: 12,
+      y: 0,
+      stepX: 70
+    }
+  });
 
-    stars.children.iterate(function (child) {
+  stars.children.iterate(function(child) {
+    //  Give each star a slightly different bounce
+    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+  });
 
-        //  Give each star a slightly different bounce
-        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+  bombs = this.physics.add.group();
 
-    });
+  //  The score
+  scoreText = this.add.text(16, 16, "score: 0", {
+    fontSize: "32px",
+    fill: "#000"
+  });
 
-    bombs = this.physics.add.group();
+  //  Collide the player and the stars with the platforms
+  this.physics.add.collider(player, platforms);
+  this.physics.add.collider(stars, platforms);
+  this.physics.add.collider(bombs, platforms);
 
-    //  The score
-    scoreText = this.add.text(16, 16, 'score: 0', {
-        fontSize: '32px',
-        fill: '#000'
-    });
+  //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
+  this.physics.add.overlap(player, stars, collectStar, null, this);
 
-    //  Collide the player and the stars with the platforms
-    this.physics.add.collider(player, platforms);
-    this.physics.add.collider(stars, platforms);
-    this.physics.add.collider(bombs, platforms);
+  this.physics.add.collider(player, bombs, hitBomb, null, this);
 
-    //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-    this.physics.add.overlap(player, stars, collectStar, null, this);
+  // Botão de ativar/desativar tela cheia
+  var button = this.add
+    .image(800 - 16, 16, "fullscreen", 0)
+    .setOrigin(1, 0)
+    .setInteractive();
 
-    this.physics.add.collider(player, bombs, hitBomb, null, this);
+  // Ao clicar no botão de tela cheia
+  button.on(
+    "pointerup",
+    function() {
+      if (this.scale.isFullscreen) {
+        button.setFrame(0);
+        this.scale.stopFullscreen();
+      } else {
+        button.setFrame(1);
+        this.scale.startFullscreen();
+      }
+    },
+    this
+  );
 
-    // Botão de ativar/desativar tela cheia
-    var button = this.add
-        .image(800 - 16, 16, "fullscreen", 0)
-        .setOrigin(1, 0)
-        .setInteractive();
-
-    // Ao clicar no botão de tela cheia
-    button.on(
-        "pointerup",
-        function () {
-            if (this.scale.isFullscreen) {
-                button.setFrame(0);
-                this.scale.stopFullscreen();
-            } else {
-                button.setFrame(1);
-                this.scale.startFullscreen();
-            }
-        },
-        this
-    );
-
-    // Tecla "F" também ativa/desativa tela cheia
-    var FKey = this.input.keyboard.addKey("F");
-    FKey.on(
-        "down",
-        function () {
-            if (this.scale.isFullscreen) {
-                button.setFrame(0);
-                this.scale.stopFullscreen();
-            } else {
-                button.setFrame(1);
-                this.scale.startFullscreen();
-            }
-        },
-        this
-    );
-
+  // Tecla "F" também ativa/desativa tela cheia
+  var FKey = this.input.keyboard.addKey("F");
+  FKey.on(
+    "down",
+    function() {
+      if (this.scale.isFullscreen) {
+        button.setFrame(0);
+        this.scale.stopFullscreen();
+      } else {
+        button.setFrame(1);
+        this.scale.startFullscreen();
+      }
+    },
+    this
+  );
 }
 
 function update() {
-    if (gameOver) {
-        return;
-    }
+  if (gameOver) {
+    return;
+  }
 
-    if (cursors.left.isDown) {
-        player.setVelocityX(-160);
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
 
-        player.anims.play('left', true);
-    } else if (cursors.right.isDown) {
-        player.setVelocityX(160);
+    player.anims.play("left", true);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(160);
 
-        player.anims.play('right', true);
-    } else {
-        player.setVelocityX(0);
+    player.anims.play("right", true);
+  } else {
+    player.setVelocityX(0);
 
-        player.anims.play('turn');
-    }
+    player.anims.play("turn");
+  }
 
-    if (cursors.up.isDown && player.body.touching.down) {
-        player.setVelocityY(-330);
-    }
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-330);
+  }
 }
 
 function collectStar(player, star) {
-    star.disableBody(true, true);
+  star.disableBody(true, true);
 
-    //  Add and update the score
-    score += 10;
-    scoreText.setText('Score: ' + score);
+  //  Add and update the score
+  score += 10;
+  scoreText.setText("Score: " + score);
 
-    if (stars.countActive(true) === 0) {
-        //  A new batch of stars to collect
-        stars.children.iterate(function (child) {
+  if (stars.countActive(true) === 0) {
+    //  A new batch of stars to collect
+    stars.children.iterate(function(child) {
+      child.enableBody(true, child.x, 0, true, true);
+    });
 
-            child.enableBody(true, child.x, 0, true, true);
+    var x =
+      player.x < 400
+        ? Phaser.Math.Between(400, 800)
+        : Phaser.Math.Between(0, 400);
 
-        });
-
-        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-
-        var bomb = bombs.create(x, 16, 'bomb');
-        bomb.setBounce(1);
-        bomb.setCollideWorldBounds(true);
-        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-        bomb.allowGravity = false;
-
-    }
+    var bomb = bombs.create(x, 16, "bomb");
+    bomb.setBounce(1);
+    bomb.setCollideWorldBounds(true);
+    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+    bomb.allowGravity = false;
+  }
 }
 
 function hitBomb(player, bomb) {
-    this.physics.pause();
+  this.physics.pause();
 
-    player.setTint(0xff0000);
+  player.setTint(0xff0000);
 
-    player.anims.play('turn');
+  player.anims.play("turn");
 
-    gameOver = true;
+  gameOver = true;
 }
