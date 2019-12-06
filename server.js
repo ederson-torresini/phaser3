b005ec914c56c5e26d8d09d510037777ee5fb7a3
Ederson Torresini <boidacarapreta@gmail.com>
2019-12-06 11:19:57 -0300

    Mover arquivos YAML para repositório pessoal Apontar usando git submodule

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-const server = require("http").Server(app);
-const port = 3000;
-
-app.use("/", express.static(__dirname));
-
-server.listen(port, () => console.log(`Server listening on port ${port}!`));
